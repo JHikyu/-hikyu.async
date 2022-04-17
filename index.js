132aec3d5ad1a9068f4dd977d74acdb0e8924de6
Justin <therosti@gmail.com>
2022-04-17 11:31:43 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,78 +1,75 @@
 const https = require('https');
-const { resolve } = require('path');
 
 let default_sleepTime = 2000;
 async function sleep(time = default_sleepTime) {
-    return new Promise(resolve => setTimeout(resolve, time));
+  return new Promise(resolve => setTimeout(resolve, time));
 }
 
 async function get(url) {
-    return new Promise((resolve, reject) => {
-        if(url.includes('http://')) reject('Please use https://');
+  return new Promise((resolve, reject) => {
+    if (url.includes('http://')) reject('Please use https://');
 
-        url = url.replace('https://', '').split(/\/(.+)/);
+    url = url.replace('https://', '').split(/\/(.+)/);
 
-        const options = {
-            hostname: url[0],
-            port: 443,
-            method: 'GET',
-            path: `/${url[1]}`,
-        };
+    const options = {
+      hostname: url[0],
+      port: 443,
+      method: 'GET',
+      path: `/${url[1]}`,
+    };
 
-        request(options).then(async data => resolve(data));
-    });
+    request(options).then(async data => resolve(data));
+  });
 }
 async function post(url) {
-    return new Promise((resolve, reject) => {
-        if(url.includes('http://')) reject('Please use https://');
+  return new Promise((resolve, reject) => {
+    if (url.includes('http://')) reject('Please use https://');
 
-        url = url.replace('https://', '').split(/\/(.+)/);
+    url = url.replace('https://', '').split(/\/(.+)/);
 
-        const options = {
-            hostname: url[0],
-            port: 443,
-            method: 'POST',
-            path: `/${url[1]}`,
-        };
+    const options = {
+      hostname: url[0],
+      port: 443,
+      method: 'POST',
+      path: `/${url[1]}`,
+    };
 
 
-        request(options).then(data => resolve(data));
-    });
+    request(options).then(data => resolve(data));
+  });
 }
 
 async function onChange(url, method, callback, interval = 10000) {
-    let currentResponse = (method === 'GET') ? await get(url) : await post(url);
+  let currentResponse = (method === 'GET') ? await get(url) : await post(url);
 
-    setInterval(async () => {
-        let response = null;
+  setInterval(async () => {
+    let response = null;
 
-        response = (method === 'GET') ? await get(url) : await post(url);
-        
-        if(response != currentResponse) {
-            currentResponse = response;
-            callback(response);
-        }
+    response = (method === 'GET') ? await get(url) : await post(url);
 
-    }, interval);
+    if (response != currentResponse) {
+      currentResponse = response;
+      callback(response);
+    }
+
+  }, interval);
 }
 
 async function request(options) {
-    return new Promise((resolve, reject) => {
-        let request = https.request(options, res => {
-            let data = '';
-            res.on('data', chunk => data += chunk);
-            res.on('end', () => resolve(data));
-        });
-        request.on('error', e => reject(e.message));
-        request.end();
+  return new Promise((resolve, reject) => {
+    let request = https.request(options, res => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => resolve(data));
     });
+    request.on('error', e => reject(e.message));
+    request.end();
+  });
 }
 
-
-
 module.exports = {
-    sleep,
-    get,
-    post,
-    onChange
+  sleep,
+  get,
+  post,
+  onChange
 };
